a792649cc3ba5fdfb834b6eee9d8206c8182e279
patryksz1021 <86234377+patryksz1021@users.noreply.github.com>
2021-09-17 20:36:39 +0200

    Add files via upload

---
+++ Gra_memory/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gra_memory
+{
+    public partial class Form1 : Form
+    {
+        int timesClick = 0;
+        Random random = new Random();
+        private Stopwatch stopWatch;
+        bool startGame = true;
+
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N","N",",",",","k","k",
+            "b", "b", "v","v","w","w","z","z"
+        };
+
+        Label firstClicked, secondClicked;
+        public Form1()
+        {
+            stopWatch = new Stopwatch();
+            InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+            timesClick++;
+            if (startGame)
+                stopWatch.Start();
+
+            startGame = false;
+
+            if (firstClicked != null && secondClicked != null)
+                return;
+
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel == null)
+                return;
+
+            if (clickedLabel.ForeColor == Color.Black)
+                return;
+
+            if(firstClicked == null)
+            {
+                firstClicked = clickedLabel;
+                firstClicked.ForeColor = Color.Black;
+                return;
+            }
+
+            secondClicked = clickedLabel;
+            secondClicked.ForeColor = Color.Black;
+
+            CheckForWinner();
+
+            if(firstClicked.Text == secondClicked.Text)
+            {
+                firstClicked = null;
+                secondClicked = null;
+            }
+            else
+                timer1.Start();
+        }
+
+        private void CheckForWinner()
+        {
+            Label label;
+            for (int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
+            {
+                label = tableLayoutPanel1.Controls[i] as Label;
+
+                if (label != null && label.ForeColor == label.BackColor)
+                    return;
+            }
+
+            stopWatch.Stop();
+            MessageBox.Show("Wygrałeś grę");
+            Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            firstClicked = null;
+            secondClicked = null;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (stopWatch.Elapsed == null)
+                label20.Text = "lol";
+            else
+                label20.Text = stopWatch.Elapsed.ToString();
+            label22.Text = timesClick.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label20_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label22_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AssignIconsToSquares()
+        {
+            Label label;
+            int randomNumber;
+
+            for (int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
+            {
+                if (tableLayoutPanel1.Controls[i] is Label)
+                    label = (Label)tableLayoutPanel1.Controls[i];
+                else
+                    continue;
+
+                randomNumber = random.Next(0, icons.Count);
+                label.Text = icons[randomNumber];
+
+                icons.RemoveAt(randomNumber);
+            }
+        }
+    }
+}
